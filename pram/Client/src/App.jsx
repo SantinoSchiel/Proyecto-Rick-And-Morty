c84ba710635b6656eb551619e0b943945a2229e3
SantinoSchiel <143773471+SantinoSchiel@users.noreply.github.com>
2023-12-07 10:56:56 -0300

    TodoOk

--- pram/Client/src/App.jsx
+++ pram/Client/src/App.jsx
@@ -108,7 +108,7 @@ function App() {
             <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
             <Route path="/about" element={<About />} />
             <Route path="/detail/:id" element={<Detail />} />
-            <Route path="/favorites" element={<Favorites/>} onClose={onClose}/>
+            <Route path="/favorites" element={<Favorites onClose={onClose}/>}/>
             <Route path="*" element={<Error />} />
          </Routes>
       </div>
