00ac3eef2ed1124949cb768362c4ca5f654616b4
SantinoSchiel <143773471+SantinoSchiel@users.noreply.github.com>
2023-12-20 23:58:04 -0300

    ultima actualizacion

--- pram/Server/src/index.js
+++ pram/Server/src/index.js
@@ -1,9 +1,24 @@
 const server = require("./app");
 let PORT = 3001;
+const { conn } = require('./DB_connection');
 
-server.listen(PORT, () => {
-   console.log('Server raised in port: ' + PORT);
+server.listen(PORT, async () => {
+   try {
+      await conn.sync({force: true});
+      console.log('Server raised in port: ' + PORT);
+   } catch (error) {
+      console.log(error.message);
+   }
 });
+
+//? Sincronizar con promesas.
+// conn.sync({force: true})
+//    .then(() => {
+//       server.listen(PORT, () => {
+//          console.log('Server raised in port: ' + PORT);
+//       })
+//    })
+//    .catch(error => console.log(error.message))
 
 
 
